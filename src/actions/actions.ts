7ace48ca624e632b8b1b987bd3ce4aced47fbb11
lucianoschwalm <luciano.rocha21@hotmail.com>
2024-10-03 22:31:56 -0300

    feat: fixing design styles and connecting pages with prisma

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,7 +1,28 @@
 "use server";
 
+import { Todo } from "@/dtos/todo";
 import db from "@/lib/db";
 
-export async function getAllTodos() {
-  await db.todo.findMany();
+export async function getAllTodos(): Promise<Todo[]> {
+  const todos = await db.todo.findMany();
+  return todos.map((todo) => {
+    return {
+      id: todo.id,
+      title: todo.title,
+      content: todo.content,
+      createdAt: todo.createdAt,
+      statusId: todo.statusId,
+      updatedAt: todo.updatedAt,
+    };
+  });
+}
+
+export async function createTodoItem(formData: FormData) {
+  await db.todo.create({
+    data: {
+      title: formData.get("title") as string,
+      content: formData.get("content") as string,
+      statusId: 1,
+    },
+  });
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 "use server";
 
+import EditIcon from "@mui/icons-material/Edit";
 import { getAllTodos } from "@/actions/actions";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
 import {
   Box,
   Button,
   Checkbox,
   Container,
-  FormGroup,
-  Paper,
+  Link,
   Table,
   TableBody,
   TableCell,
@@ -15,59 +17,30 @@ import {
   TableRow,
   Typography,
 } from "@mui/material";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { Todo } from "@/dtos/todo";
 
 export default async function Home() {
-  // const allUsers = await getAllTodos();
-  const columns: GridColDef[] = [
-    { field: "id", headerName: "ID", width: 70 },
-    { field: "firstName", headerName: "First name", width: 130 },
-    { field: "lastName", headerName: "Last name", width: 130 },
-    {
-      field: "age",
-      headerName: "Age",
-      type: "number",
-      width: 90,
-    },
-    {
-      field: "fullName",
-      headerName: "Full name",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
-      width: 160,
-    },
-  ];
+  const todosItems = await getAllTodos();
 
-  const rows = [
-    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  ];
-
-  const paginationModel = { page: 0, pageSize: 5 };
   return (
     <Box className="pt-10 pl-0 lg:pl-10 min-h-screen bg-gray-300">
       <div className="grid grid-cols-1 lg:grid-cols-3 pt-10">
         <Typography
           variant="h3"
-          className="text-center col-start-1 lg:col-start-2 pl-0 pb-0 lg:pb-10 text-black"
+          className="text-center col-start-1 lg:col-start-2 pl-0 text-black"
         >
           Todo List!
         </Typography>
-        <div className="text-center ">
-          <Button
-            variant="outlined"
-            className="bg-transparent hover:bg-indigo-500 text-indigo-700 font-semibold hover:text-white py-2 px-4 border border-indigo-500 hover:border-transparent rounded"
-          >
-            + Add a new Todo Item
-          </Button>
-        </div>
+        <Link href={"/create"}>
+          <div className="text-center">
+            <Button
+              variant="outlined"
+              className="bg-transparent hover:bg-indigo-500 text-indigo-700 font-semibold hover:text-white py-2 px-4 border border-indigo-500 hover:border-transparent rounded"
+            >
+              + Add a new Todo Item
+            </Button>
+          </div>
+        </Link>
       </div>
       <Container
         sx={{
@@ -75,42 +48,72 @@ export default async function Home() {
           justifyContent: "center",
         }}
       >
-        <form className="w-full py-20 flex justify-center pt-10">
-          <Box className="w-full lg:w-3/4">
+        <Box className="w-full py-20 flex justify-center pt-10">
+          <Box className="w-full lg:w-full">
             <Box className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               <Box className="sm:col-span-6">
                 <Table className="bg-gray-300 text-black text-left">
                   <TableHead>
                     <TableRow>
-                      <Checkbox
-                        color="primary"
-                        className="pt-4"
-                        // checked={isItemSelected}
-                      />
+                      <TableCell />
                       <TableCell>Title</TableCell>
-                      <TableCell>Content</TableCell>
+                      <TableCell>Description</TableCell>
                       <TableCell>Date of Creation</TableCell>
                       <TableCell>Status</TableCell>
                     </TableRow>
                   </TableHead>
-                  <TableBody>
-                    <TableRow>
-                      <Checkbox
-                        color="primary"
-                        className="pt-4"
-                        // checked={isItemSelected}
-                      />
+                  {todosItems.map((todoItem: Todo) => (
+                    <TableBody key={todoItem.id}>
+                      <TableRow>
+                        <TableCell>
+                          <Checkbox
+                            color="primary"
+                            className="pt-4"
+                            icon={<RadioButtonUncheckedIcon />}
+                            checkedIcon={<CheckCircleIcon />}
+                            disabled
+                            // checked={isItemSelected}
+                          />
+                        </TableCell>
+                        <TableCell>{todoItem.title}</TableCell>
+                        <TableCell>{todoItem.content}</TableCell>
+                        <TableCell>
+                          {todoItem.createdAt.toLocaleDateString()}
+                        </TableCell>
+                        <TableCell>{todoItem.statusId}</TableCell>
+                        <TableCell>
+                          <Button type="submit" color="inherit">
+                            <EditIcon />
+                          </Button>
+                        </TableCell>
+                      </TableRow>
+                    </TableBody>
+                  ))}
+                  {/* <TableRow>
+                      <TableCell>
+                        <Checkbox
+                          color="primary"
+                          className="pt-4"
+                          icon={<RadioButtonUncheckedIcon />}
+                          checkedIcon={<CheckCircleIcon />}
+                          // checked={isItemSelected}
+                        />
+                      </TableCell>
                       <TableCell>Walk in the plaza</TableCell>
                       <TableCell>Im gonna walk in central park in NY</TableCell>
                       <TableCell>09/15/2024</TableCell>
-                      <TableCell>Created</TableCell>
-                    </TableRow>
-                  </TableBody>
+                      <TableCell>Pending</TableCell>
+                      <TableCell>
+                        <Button type="submit" color="inherit">
+                          <EditIcon />
+                        </Button>
+                      </TableCell>
+                    </TableRow> */}
                 </Table>
               </Box>
             </Box>
           </Box>
-        </form>
+        </Box>
       </Container>
     </Box>
   );
