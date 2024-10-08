60467f294d5cc3a12d0fd216a7601331cdadb4ca
lucianoschwalm <luciano.rocha21@hotmail.com>
2024-10-08 14:42:20 -0300

    feat: deleting useless code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
 import { ReactQueryProvider } from "./react-query-provider";
 
 const inter = Inter({ subsets: ["latin"] });
